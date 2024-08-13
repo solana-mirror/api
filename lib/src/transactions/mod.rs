00a7b31c42b307aeab80516eedf86f573059fff2
setPedro <pedross4789@gmail.com>
2024-08-13 13:56:47 +0200

    fix: handle more than 1000 txs

--- lib/src/transactions/mod.rs
+++ lib/src/transactions/mod.rs
@@ -1,8 +1,9 @@
 use std::{collections::HashMap, str::FromStr};
-use solana_client::rpc_client::RpcClient;
+use solana_client::{rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient}, rpc_config::RpcTransactionConfig};
 use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signature::Signature;
-use solana_transaction_status::{EncodedConfirmedTransactionWithStatusMeta, UiTransactionEncoding};
+use solana_transaction_status::EncodedConfirmedTransactionWithStatusMeta;
+use solana_sdk::commitment_config::CommitmentConfig;
 
 use crate::{transactions::types::{BalanceChange, FormattedAmount, GetTransactionsError, ParsedTransaction}, utils::get_rpc};
 
@@ -10,15 +11,41 @@ pub mod types;
 
 fn get_signatures(connection: &RpcClient, address: &str) -> Result<Vec<String>, GetTransactionsError> {
     let pubkey = Pubkey::from_str(&address);
+    let mut before: Option<Signature> = None;
+    let mut should_continue: bool = true;
+    let mut signatures: Vec<String> = Vec::new();
 
     match pubkey {
         Ok(pubkey) => {
-            let signatures = connection.get_signatures_for_address(&pubkey);
-            
-            match signatures {
-                Ok(signatures) => Ok(signatures.into_iter().map(|x| x.signature).collect()),
-                Err(_) => Err(GetTransactionsError::FetchError),
+            while should_continue {
+                let raw_signatures = connection.get_signatures_for_address_with_config(&pubkey, GetConfirmedSignaturesForAddress2Config {
+                    before: before.clone(),
+                    until: None,
+                    limit: None,
+                    commitment: Some(CommitmentConfig::confirmed()),
+                }).unwrap_or([].to_vec());
+
+                if raw_signatures.is_empty() {
+                    should_continue = false;
+                } else {
+                    let mapped: Vec<String> = raw_signatures
+                        .iter()
+                        .map(|x| x.signature.clone())
+                        .collect();
+
+                    if let Some(last_signature) = mapped.last() {
+                        before = Some(Signature::from_str(last_signature).unwrap());
+                    }
+
+                    signatures.extend(mapped);
+
+                    if raw_signatures.len() != 1000 {
+                        should_continue = false;
+                    }
+                }
             }
+            
+            Ok(signatures)
         }
         Err(_) => Err(GetTransactionsError::InvalidAddress),
     }
@@ -26,25 +53,22 @@ fn get_signatures(connection: &RpcClient, address: &str) -> Result<Vec<String>, 
 
 async fn get_confirmed_transaction(connection: &RpcClient, signature: &str) -> Result<EncodedConfirmedTransactionWithStatusMeta, GetTransactionsError> {
     let sign = Signature::from_str(signature).unwrap();
-    connection.get_transaction(&sign, UiTransactionEncoding::Base64)
+    connection.get_transaction_with_config(&sign, RpcTransactionConfig {
+        encoding: None,
+        commitment: None,
+        max_supported_transaction_version: Some(0)
+    })
         .map_err(|_| GetTransactionsError::FetchError)
 }
 
 pub async fn get_parsed_transactions(address: String) -> Result<Vec<ParsedTransaction>, GetTransactionsError> {
     let connection = RpcClient::new(get_rpc());
-    let signatures = match get_signatures(&connection, &address) {
-         Ok(sigs) => sigs,
-         Err(err) => return Err(err),
-    };
+    let signatures = get_signatures(&connection, &address).unwrap();
 
-    // (only one tx) -> TODO: handle all txs (too many requests)
-    let sig = &signatures[0];
-    let future = async move {
-        get_confirmed_transaction(&connection, sig).await
-    };
-    let result: Result<EncodedConfirmedTransactionWithStatusMeta, GetTransactionsError> = future.await;
-   
-    println!("{:?}", result);
+    // handle get_confirmed_transaction for all signatures
+    let transactions = get_confirmed_transaction(&connection, &signatures[0]).await.unwrap();
+
+    println!("{:?}", transactions);
 
     // mock data
     let transaction1 = ParsedTransaction {
