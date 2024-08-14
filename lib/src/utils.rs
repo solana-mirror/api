a3ff2aece80dfc37331d11d1293ad409fbd2565e
config.json <configjson@proton.me>
2024-08-14 16:48:31 +0300

    fix: dotenv

--- lib/src/utils.rs
+++ lib/src/utils.rs
@@ -1,14 +1,15 @@
-use std::env;
-
+use dotenv::dotenv;
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{program_pack::Pack, pubkey::Pubkey};
 use spl_token::state::Mint;
+use std::env;
 
 pub fn clean_string(s: String) -> String {
     s.trim_matches('\0').trim_matches('"').to_string()
 }
 
 pub fn get_rpc() -> String {
+    dotenv().ok();
     env::var("RPC").unwrap_or_else(|_| "https://api.mainnet-beta.solana.com".to_string())
 }
 
