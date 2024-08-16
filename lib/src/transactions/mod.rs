c23e5a615e37287cefd1e1aa0a6bab4054c13287
setPedro <pedross4789@gmail.com>
2024-08-16 14:30:17 +0200

    refactor: parse_transaction fn

--- lib/src/transactions/mod.rs
+++ lib/src/transactions/mod.rs
@@ -98,94 +98,91 @@ fn parse_transaction(
     let mut balances: HashMap<String, BalanceChange> = HashMap::new();
     let tx = transaction.result.clone().unwrap();
 
-    let owner_idx = tx
+    let owner_idx = match tx
         .transaction
         .message
         .account_keys
         .iter()
-        .position(|x| x.to_owned() == signer.to_string());
-
-    match owner_idx {
-        Some(idx) => {
-            // Handle SOL
-            let pre_sol = tx.meta.pre_balances[idx];
-            let post_sol = tx.meta.post_balances[idx];
-
-            if pre_sol != post_sol {
-                balances.insert(
-                    SOL_ADDRESS.to_string(),
-                    BalanceChange {
-                        pre: FormattedAmount {
-                            amount: pre_sol,
-                            formatted: pre_sol as f64 / LAMPORTS_PER_SOL as f64,
-                        },
-                        post: FormattedAmount {
-                            amount: post_sol,
-                            formatted: post_sol as f64 / LAMPORTS_PER_SOL as f64,
-                        },
-                    },
-                );
-            }
-
-            // Handle SPL
-            let pre_token_balances: Vec<TokenBalance> = tx
-                .clone()
-                .meta
-                .pre_token_balances
-                .into_iter()
-                .filter(|x| x.owner == signer.to_string())
-                .collect();
-
-            let post_token_balances: Vec<TokenBalance> = tx
-                .clone()
-                .meta
-                .post_token_balances
-                .into_iter()
-                .filter(|x| x.owner == signer.to_string())
-                .collect();
-
-            for pre_balance in pre_token_balances {
-                if !balances.contains_key(pre_balance.mint.as_str()) {
-                    balances.insert(pre_balance.mint.to_string(), BalanceChange::default());
-                }
-
-                let balance_change = balances.get_mut(pre_balance.mint.as_str()).unwrap();
-                balance_change.pre = FormattedAmount {
-                    amount: pre_balance.ui_token_amount.amount.parse::<u64>().unwrap(),
-                    formatted: pre_balance.ui_token_amount.ui_amount.unwrap_or_default(),
-                };
-            }
-
-            for post_balance in post_token_balances {
-                if !balances.contains_key(post_balance.mint.as_str()) {
-                    balances.insert(post_balance.mint.to_string(), BalanceChange::default());
-                }
-
-                let balance_change = balances.get_mut(post_balance.mint.as_str()).unwrap();
-                balance_change.post = FormattedAmount {
-                    amount: post_balance.ui_token_amount.amount.parse::<u64>().unwrap(),
-                    formatted: post_balance.ui_token_amount.ui_amount.unwrap_or_default(),
-                };
-            }
-
-            // Handle ixs
-            let parsed_instructions: Vec<String> = tx
-                .meta
-                .log_messages
-                .clone()
-                .unwrap_or_default()
-                .into_iter()
-                .filter(|x| x.starts_with("Program log: Instruction: "))
-                .map(|x| x.replace("Program log: Instruction: ", ""))
-                .collect();
-
-            Ok(ParsedTransaction {
-                block_time: tx.block_time.unwrap_or_default(),
-                signatures: tx.transaction.signatures,
-                balances,
-                parsed_instructions,
-            })
-        }
+        .position(|x| x.to_owned() == signer.to_string())
+    {
+        Some(idx) => idx,
         None => return Err(Error::InvalidAddress),
+    };
+
+    // Handle SOL
+    let pre_sol = tx.meta.pre_balances[owner_idx];
+    let post_sol = tx.meta.post_balances[owner_idx];
+
+    if pre_sol != post_sol {
+        balances.insert(
+            SOL_ADDRESS.to_string(),
+            BalanceChange {
+                pre: FormattedAmount {
+                    amount: pre_sol,
+                    formatted: pre_sol as f64 / LAMPORTS_PER_SOL as f64,
+                },
+                post: FormattedAmount {
+                    amount: post_sol,
+                    formatted: post_sol as f64 / LAMPORTS_PER_SOL as f64,
+                },
+            },
+        );
     }
+
+    // Handle SPL
+    let pre_token_balances: Vec<TokenBalance> = tx
+        .meta
+        .pre_token_balances
+        .into_iter()
+        .filter(|x| x.owner == signer.to_string())
+        .collect();
+
+    let post_token_balances: Vec<TokenBalance> = tx
+        .meta
+        .post_token_balances
+        .into_iter()
+        .filter(|x| x.owner == signer.to_string())
+        .collect();
+
+    for pre_balance in pre_token_balances {
+        if !balances.contains_key(pre_balance.mint.as_str()) {
+            balances.insert(pre_balance.mint.to_string(), BalanceChange::default());
+        }
+
+        let balance_change = balances.get_mut(pre_balance.mint.as_str()).unwrap();
+        balance_change.pre = FormattedAmount {
+            amount: pre_balance.ui_token_amount.amount.parse::<u64>().unwrap(),
+            formatted: pre_balance.ui_token_amount.ui_amount.unwrap_or_default(),
+        };
+    }
+
+    for post_balance in post_token_balances {
+        if !balances.contains_key(post_balance.mint.as_str()) {
+            balances.insert(post_balance.mint.to_string(), BalanceChange::default());
+        }
+
+        let balance_change = balances.get_mut(post_balance.mint.as_str()).unwrap();
+        balance_change.post = FormattedAmount {
+            amount: post_balance.ui_token_amount.amount.parse::<u64>().unwrap(),
+            formatted: post_balance.ui_token_amount.ui_amount.unwrap_or_default(),
+        };
+    }
+
+    // Handle ixs
+    let parsed_instructions: Vec<String> = tx
+        .meta
+        .log_messages
+        .clone()
+        .unwrap_or_default()
+        .into_iter()
+        .filter(|x| x.starts_with("Program log: Instruction: "))
+        .map(|x| x.replace("Program log: Instruction: ", ""))
+        .collect();
+
+    Ok(ParsedTransaction {
+        block_time: tx.block_time.unwrap_or_default(),
+        signatures: tx.transaction.signatures,
+        balances,
+        parsed_instructions,
+    })
 }
